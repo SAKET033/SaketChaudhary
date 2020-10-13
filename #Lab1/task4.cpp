4c6ad5fcb13539f27e49c858db34f0edad01e9de
SAKET033 <72693969+SAKET033@users.noreply.github.com>
2020-10-13 13:27:24 +0545

    Update task4.cpp

--- #Lab1/task4.cpp
+++ #Lab1/task4.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 inline int Max(int x, int y)
 {
-	cout<<"Entered numbers are "<<x<<y;
+	cout<<"Entered numbers are :"<<x<<y;
 	if(x>y)
 	{
 		return x;
@@ -18,16 +18,16 @@ inline int Max(int x, int y)
 }
 int main()
 {
-	int a[15],n;
-	int p,q;
+	int a[20],n;
+	int r,s;
 	cout<<"Enter how many time the number to be check:"<<endl;
 	cin>>n;
 	for(int i=0;i<n;i++)
 	{
-		cout<<"Enter x and y"<<endl;
-		cin>>p>>q;
-		a[i]=max(p,q);
-		cout<<"Max value is "<<a[i]<<endl;
+		cout<<"Enter x and y:"<<endl;
+		cin>>r>>s;
+		a[i]=max(r,s);
+		cout<<"Max value is : "<<a[i]<<endl;
 
 	}
 
