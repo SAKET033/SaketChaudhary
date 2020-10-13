3327ed9dba14c75b7abfa111b9a4a22f73c386cb
SAKET033 <72693969+SAKET033@users.noreply.github.com>
2020-10-13 13:18:57 +0545

    Update task3.cpp

--- #Lab1/task3.cpp
+++ #Lab1/task3.cpp
@@ -8,31 +8,31 @@ class Area
 		void calc_area(int r)
 	{
 
-		cout<<"Area of circle is "<<3.14*r*r<<endl;
+		cout<<"Area of circle is: "<<3.14*r*r<<endl;
 
 
 	}
 	void calc_area(int l,int b)
 	{
-		cout<<"Area of rectangle is "<<l*b<<endl;
+		cout<<"Area of rectangle is: "<<l*b<<endl;
 
 
 	}
 	void choose()
 	{
 		int n,r,l,b;
-		cout<<"Enter 1 to calculate area of circle and 2 for calculating area of rectangle"<<endl;
+		cout<<"Enter 0 to calculate area of circle and 1 for calculating area of rectangle:"<<endl;
 		cin>>n;
-		if(n==1)
+		if(n==0)
 		{
-			cout<<"Enter radius"<<endl;
+			cout<<"Enter radius:"<<endl;
 			cin>>r;
 			calc_area(r);
 
 		}
-		if(n==2)
+		if(n==1)
 		{
-			cout<<"Enter length and breath"<<endl;
+			cout<<"Enter length and breath:"<<endl;
 			cin>>l>>b;
 			calc_area(l,b);
 		}
